ba832c39f1dd1e026a8a72b502475340e667c2cb
van Meer, Ronald <ronaldvanmeer@gmail.com>
2019-05-14 12:09:18 +0200

    Create of separate project binding on ID instead of object type LocationViewModel

---
+++ TestComplexGrid1/TestComplexGrid/ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestComplexGrid.ViewModels
+{
+    public class DepartmentViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Department { get; set; }
+        [Required]
+        [UIHint("LocationSelector")]
+        public int LocationId { get; set; }
+        public LocationViewModel Location { get; set; }
+        public bool Active { get; set; }
+
+    }
+}
